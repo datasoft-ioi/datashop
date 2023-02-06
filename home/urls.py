80cf361f6af2490ddf550bf1d125b8d3ee9896ca
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-02-06 16:33:01 +0500

    productDtl Html structre

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage),
-    path('cart/', views.cartpage) 
+    path('cart/', views.cartpage) ,
+    path('detail/', views.productdtl) 
 ]
