42d342071882077122cac0796743796d21d9c158
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-02-07 16:57:57 +0500

    category list html structura fulled

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage),
     path('cart/', views.cartpage) ,
-    path('detail/', views.productdtl)
+    path('detail/', views.productdtl),
+    path('categoryList/', views.category_list)
 ]
