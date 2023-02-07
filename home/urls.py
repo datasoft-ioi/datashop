a124dba115a0757b7725d9f66a6bbe48624ddbed
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-02-07 18:01:08 +0500

    Akbar (#5)
    * user, form, product
    
    * product, order, home
    
    * products backend call html

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
+    path('', views.index, name="index"),
     path('cart/', views.cartpage) ,
     path('detail/', views.productdtl)
 ]
