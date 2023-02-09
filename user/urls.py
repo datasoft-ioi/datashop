6f397476001d5c1480b74c8e2099f7337a7d2a0a
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-02-09 17:59:42 +0500

    Akbar login, auath (#11)
    * detail, cat, bruhh
    
    * url add
    
    * login, auth

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+#app_name = 'user'
+urlpatterns = [
+    path('', views.index, name='user_index'),
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.user_password, name='user_password'),
+    path('orders/', views.user_orders, name='user_orders'),
+    path('orders_product/', views.user_order_product, name='user_order_product'),
+    path('orderdetail/<int:id>', views.user_orderdetail, name='user_orderdetail'),
+    path('order_product_detail/<int:id>/<int:oid>', views.user_order_product_detail, name='user_order_product_detail'),
+    path('comments/', views.user_comments, name='user_comments'),
+    path('deletecomment/<int:id>', views.user_deletecomment, name='user_deletecomment'),
+
+]
