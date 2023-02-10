207250bf5f4631270d6b994b0d05d8f0787a631a
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-02-11 02:20:03 +0500

    bug static_files

--- datashop/urls.py
+++ datashop/urls.py
@@ -49,3 +49,5 @@ urlpatterns += i18n_patterns(
     path('ckeditor/', include('ckeditor_uploader.urls')),
     prefix_default_language=False,
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
