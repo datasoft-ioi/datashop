3c3fadf5330c9b837763e45cb1e053ba24e028be
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-02-14 21:55:57 +0500

    Akbar localhost (#18)
    * detail, cat, bruhh
    
    * url add
    
    * login, auth
    
    * cart, links add future
    
    * ckeditor_uploader
    
    * debug=false, allowed hosts add datashop.uz
    
    * true'
    
    * static_root file
    
    * bug static_files
    
    * fuck static files admin
    
    * settings debug uer
    
    * fix bugs
    
    * media toor
    
    * false
    
    * all allowed hosts
    
    * debug tru
    
    * allowed hosts add
    
    * night catlist bug
    
    * catalog navbar, cat srt
    
    * static files fix bug production
    
    * localhost aloowed hosts

--- home/views.py
+++ home/views.py
@@ -52,8 +52,7 @@ def index(request):
     products_picked = Product.objects.all().order_by('?')[:4]   #Random selected 4 products
 
     category = Category.objects.all()
-
-    category_latest = Category.objects.all().order_by('?')[:4] #Random selected
+    category_lates = Category.objects.all().order_by('?')[:4] #Random selected
 
 
     page="home"
@@ -63,7 +62,7 @@ def index(request):
              'products_latest': products_latest,
              'products_picked': products_picked,
              'category':category,
-             'category_latest': category_latest,
+             'category_lates':category_lates,
 
              }
     return render(request,'index.html',context)
@@ -122,8 +121,7 @@ def category_products(request,id,slug):
     defaultlang = settings.LANGUAGE_CODE[0:2]
     # currentlang = request.LANGUAGE_CODE[0:2]
     catdata = Category.objects.get(pk=id)
-    category = Category.objects.filter(parent=None)
-
+    category = Category.objects.all()
     products = Product.objects.filter(category_id=id) #default language
     # if defaultlang != currentlang:
     #     try:
