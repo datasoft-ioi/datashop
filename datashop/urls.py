0fa866aa11e5f4452294e55fc854932ad65aab4e
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-02-15 13:09:30 +0500

    Fix bug debugs

--- datashop/urls.py
+++ datashop/urls.py
@@ -50,11 +50,12 @@ urlpatterns += i18n_patterns(
     prefix_default_language=False,
 ) 
 
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # if settings.DEBUG:
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
