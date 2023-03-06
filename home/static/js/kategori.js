78e79d14c627f56ce4e1cacdc545c2907dec435b
alibek-jumaniyazov <alibekjumaniyazov007@gmail.com>
2023-03-06 18:20:46 +0500

    kategory add function

--- home/static/js/kategori.js
+++ home/static/js/kategori.js
@@ -4,22 +4,45 @@ const close = document.querySelector('.fa-xmark')
 const filterMenu = document.querySelector('.categoryListMainFilter')
 const body = document.querySelector('body')
 
-const aksesuar = document.querySelector('.aksesuar').onmouseenter = fun1
-const elektronika = document.querySelector('.elektronika').onmouseenter = fun2
-const text2 = document.querySelector('.victus')
-const text1 = document.querySelector('.notebook')
+const cursor1 = document.querySelector('.oyin').onmouseenter = fun1
+const cursor2 = document.querySelector('.notebook').onmouseenter = fun2
+const cursor3 = document.querySelector('.gajed').onmouseenter = fun3
+const cursor4 = document.querySelector('.audio').onmouseenter = fun4
+
+const text1 = document.querySelector('.geymerlar')
+const text2 = document.querySelector('.noutbuk')
+const text3 = document.querySelector('.telefon')
+const text4 = document.querySelector('.televizor')
 
 
 function fun1(){
-    text2.style.display = 'block'
-    text1.style.display = 'none'
-
+    text1.style.display = 'block'
+    text2.style.display = 'none'
+    text3.style.display = 'none'
+    text4.style.display = 'none'
 }
 
 function fun2(){
-    text1.style.display = 'block'
-    text2.style.display = 'none'
+    text2.style.display = 'block'
+    text1.style.display = 'none'
+    text3.style.display = 'none'
+    text4.style.display = 'none'
 }
+
+function fun3(){
+    text3.style.display = 'block'
+    text1.style.display = 'none'
+    text2.style.display = 'none'
+    text4.style.display = 'none'
+}
+
+function fun4(){
+    text4.style.display = 'block'
+    text1.style.display = 'none'
+    text2.style.display = 'none'
+    text3.style.display = 'none'
+}
+
 
 
 
