ca322e5f13dc29a5a0ab33982863e736d17c93cf
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-04-10 08:27:14 +0500

    IndexView, UserLoginView...., Mixins

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from . import views
+from .views import ProductsListView, basket, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.products, name='index'),
-    path('category/<int:category_id>/', views.products, name='category'),
-    path('page/<int:page>/', views.products, name='paginator'),
-    path('savat/', views.basket, name='basket'),
-    path('basket/add/<int:product_id>/', views.basket_add, name='basket_add'),
-    path('basket/remove/<int:basket_id>/', views.basket_remove, name='basket_remove'),
+    path('', ProductsListView.as_view(), name='index'),
+    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
+    path('savat/', basket, name='basket'),
+    path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
-
+from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 from django.contrib import auth, messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def login(request):
@@ -30,37 +32,74 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Tabriklaymiz muaffokiyatli registrationdan ottingiz')
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm()
-    context = {
-        "form": form,
-    }
-    return render(request, 'users/registration.html', context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
-@login_required
-def profile(request):
+class UserRegristrationView(SuccessMessageMixin, CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+    success_message = "Tabriklaymiz muaffokiyatli registrationdan ottingiz"
 
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user)
+    def get_context_data(self, **kwargs):
+        context = super(UserRegristrationView, self).get_context_data()
+        context['title'] = "Datashop - User Registrations"
+        return context
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = "Datashop - User Profile"
+        # context['basket'] = Basket.objects.filter(user=self.object)
+        return context
+
+
+
+
+# @login_required
+# def profile(request):
+
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserProfileForm(instance=request.user)
     
-    context = {
+#     context = {
 
-        "title": "Profile",
-        "form": form,
-    }
-    return render(request, 'users/profile.html', context)
+#         "title": "Profile",
+#         "form": form,
+#     }
+#     return render(request, 'users/profile.html', context)
+
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Tabriklaymiz muaffokiyatli registrationdan ottingiz')
+#             return HttpResponseRedirect(reverse('users:login'))
+#     else:
+#         form = UserRegistrationForm()
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, 'users/registration.html', context)
