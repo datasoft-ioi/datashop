ed7308e2d703f33f0e2c746ef7ec6c12e5f0d352
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-04-23 00:28:14 +0500

    Debug False "zabil" :angry:
    suka

--- store/settings.py
+++ store/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["datashop.uz", ".datashop.uz", "localhost", "127.0.0.1"]
 
